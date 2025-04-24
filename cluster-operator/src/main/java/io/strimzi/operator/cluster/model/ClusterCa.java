42b27a0bcc73d446b7360e18d718e748ffae30ee
Bennett Ellis <beelli@microsoft.com>
2025-04-24 00:05:01 -0700

    fix clusterca

--- cluster-operator/src/main/java/io/strimzi/operator/cluster/model/ClusterCa.java
+++ cluster-operator/src/main/java/io/strimzi/operator/cluster/model/ClusterCa.java
@@ -9,14 +9,21 @@ import io.strimzi.api.kafka.model.common.CertificateAuthority;
 import io.strimzi.api.kafka.model.common.CertificateExpirationPolicy;
 import io.strimzi.certs.CertAndKey;
 import io.strimzi.certs.CertManager;
+import io.strimzi.certs.Subject;
 import io.strimzi.operator.common.Reconciliation;
 import io.strimzi.operator.common.model.Ca;
 import io.strimzi.operator.common.model.PasswordGenerator;
 
 import java.io.IOException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * Represents the Cluster CA
@@ -157,6 +164,161 @@ public class ClusterCa extends Ca {
     @Override
     protected String caCertGenerationAnnotation() {
         return ANNO_STRIMZI_IO_CLUSTER_CA_CERT_GENERATION;
+    }
+
+    /**
+     * Copy already existing certificates from provided Secret based on number of effective replicas
+     * and maybe generate new ones for new replicas (i.e. scale-up).
+     *
+     * @param reconciliation                        Reconciliation marker
+     * @param nodes                                 List of nodes for which the certificates should be generated
+     * @param subjectFn                             Function to generate certificate subject for given node / pod
+     * @param secret                                Secret with certificates
+     * @param isMaintenanceTimeWindowsSatisfied     Flag indicating if we are inside a maintenance window or not
+     *
+     * @return  Returns map with node certificates which can be used to create or update the certificate secret
+     *
+     * @throws IOException  Throws IOException when working with files fails
+     */
+    /* test */ Map<String, CertAndKey> maybeCopyOrGenerateCerts(
+        Reconciliation reconciliation,
+        Set<NodeRef> nodes,
+        Function<NodeRef, Subject> subjectFn,
+        Secret secret,
+        boolean isMaintenanceTimeWindowsSatisfied
+    ) throws IOException {
+        // Maps for storing the certificates => will be used in the new or updated secret. This map is filled in this method and returned at the end.
+        Map<String, CertAndKey> certs = new HashMap<>();
+
+        // Temp files used when we need to generate new certificates
+        File brokerCsrFile = Files.createTempFile("tls", "broker-csr").toFile();
+        File brokerKeyFile = Files.createTempFile("tls", "broker-key").toFile();
+        File brokerCertFile = Files.createTempFile("tls", "broker-cert").toFile();
+        File brokerKeyStoreFile = Files.createTempFile("tls", "broker-p12").toFile();
+
+        for (NodeRef node : nodes)  {
+            String podName = node.podName();
+            Subject subject = subjectFn.apply(node);
+
+            if (!this.certRenewed() // No CA renewal is happening
+                    && secret != null && secret.getData() != null // Secret exists and has some data
+                    && secretEntryExists(secret, podName, SecretEntry.CRT) // The secret has the public key for this pod
+                    && secretEntryExists(secret, podName, SecretEntry.KEY) // The secret has the private key for this pod
+                    && !hasCaCertGenerationChanged(secret) // The generation on the Secret is the same as the CA has
+            )   {
+                // A certificate for this node already exists, so we will try to reuse it
+                LOGGER.debugCr(reconciliation, "Certificate for node {} already exists", node);
+
+                CertAndKey certAndKey = asCertAndKey(secret.getData(), podName);
+
+                List<String> reasons = new ArrayList<>(2);
+
+                if (certSubjectChanged(certAndKey, subject, podName))   {
+                    reasons.add("DNS names changed");
+                }
+
+                if (isExpiring(secret, podName + ".crt") && isMaintenanceTimeWindowsSatisfied)  {
+                    reasons.add("certificate is expiring");
+                }
+
+                if (renewalType.equals(RenewalType.CREATE)) {
+                    reasons.add("certificate added");
+                }
+
+                if (!reasons.isEmpty())  {
+                    LOGGER.infoCr(reconciliation, "Certificate for pod {} need to be regenerated because: {}", podName, String.join(", ", reasons));
+
+                    CertAndKey newCertAndKey = generateSignedCert(subject, brokerCsrFile, brokerKeyFile, brokerCertFile, brokerKeyStoreFile);
+                    certs.put(podName, newCertAndKey);
+                }   else {
+                    certs.put(podName, certAndKey);
+                }
+            } else {
+                // A certificate for this node does not exist or it the CA got renewed, so we will generate new certificate
+                LOGGER.debugCr(reconciliation, "Generating new certificate for node {}", node);
+                CertAndKey k = generateSignedCert(subject, brokerCsrFile, brokerKeyFile, brokerCertFile, brokerKeyStoreFile);
+                certs.put(podName, k);
+            }
+        }
+
+        // Delete the temp files used to generate new certificates
+        delete(reconciliation, brokerCsrFile);
+        delete(reconciliation, brokerKeyFile);
+        delete(reconciliation, brokerCertFile);
+        delete(reconciliation, brokerKeyStoreFile);
+
+        return certs;
+    }
+
+    /**
+     * Return given secret for pod as a CertAndKey object
+     *
+     * @param certificateData   The Map with the certificate data from the Kubernetes Secret(s)
+     * @param podName           Name of the pod
+     *
+     * @return  CertAndKey instance
+     */
+    private static CertAndKey asCertAndKey(Map<String, String> certificateData, String podName) {
+        String keyData = certificateData.get(SecretEntry.KEY.asKey(podName));
+        if (keyData == null) {
+            throw new RuntimeException("Certificate for node " + podName + " is missing the private key");
+        }
+
+        String certData = certificateData.get(SecretEntry.CRT.asKey(podName));
+        if (certData == null) {
+            throw new RuntimeException("Certificate for node " + podName + " is missing the public key");
+        }
+
+        return new CertAndKey(Util.decodeBytesFromBase64(keyData), Util.decodeBytesFromBase64(certData));
+    }
+
+    /**
+     * Checks whether subject alternate names changed and certificate needs a renewal
+     *
+     * @param certAndKey        Current certificate
+     * @param desiredSubject    Desired subject alternate names
+     * @param podName           Name of the pod to which this certificate belongs (used for log messages)
+     *
+     * @return  True if the subjects are different, false otherwise
+     */
+    /* test */ boolean certSubjectChanged(CertAndKey certAndKey, Subject desiredSubject, String podName)    {
+        Collection<String> desiredAltNames = desiredSubject.subjectAltNames().values();
+        Collection<String> currentAltNames = getSubjectAltNames(certAndKey.cert());
+
+        if (currentAltNames != null && desiredAltNames.containsAll(currentAltNames) && currentAltNames.containsAll(desiredAltNames))   {
+            LOGGER.traceCr(reconciliation, "Alternate subjects match. No need to refresh cert for pod {}.", podName);
+            return false;
+        } else {
+            LOGGER.infoCr(reconciliation, "Alternate subjects for pod {} differ", podName);
+            LOGGER.infoCr(reconciliation, "Current alternate subjects: {}", currentAltNames);
+            LOGGER.infoCr(reconciliation, "Desired alternate subjects: {}", desiredAltNames);
+            return true;
+        }
+    }
+
+    /**
+     * Extracts the alternate subject names out of existing certificate
+     *
+     * @param certificate   Existing X509 certificate as a byte array
+     *
+     * @return  List of certificate Subject Alternate Names
+     */
+    private List<String> getSubjectAltNames(byte[] certificate) {
+        List<String> subjectAltNames = null;
+
+        try {
+            X509Certificate cert = x509Certificate(certificate);
+            Collection<List<?>> altNames = cert.getSubjectAlternativeNames();
+            subjectAltNames = altNames.stream()
+                    .filter(name -> name.get(1) instanceof String)
+                    .map(item -> (String) item.get(1))
+                    .collect(Collectors.toList());
+        } catch (CertificateException | RuntimeException e) {
+            // TODO: We should mock the certificates properly so that this doesn't fail in tests (not now => long term :-o)
+            LOGGER.debugCr(reconciliation, "Failed to parse existing certificate", e);
+        }
+
+        return subjectAltNames;
     }
 
 
