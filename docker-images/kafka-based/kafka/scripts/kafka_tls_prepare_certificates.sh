0a2460b3e69f06516120cdf84fa18439fce6d805
Bennett Ellis <beelli@microsoft.com>
2025-04-25 11:27:15 -0700

    update kafka image tls cert prep

--- docker-images/kafka-based/kafka/scripts/kafka_tls_prepare_certificates.sh
+++ docker-images/kafka-based/kafka/scripts/kafka_tls_prepare_certificates.sh
@@ -1,75 +1,62 @@
 #!/usr/bin/env bash
 set -e
 
-# Load predefined functions for preparing trust- and keystores
-source ./tls_utils.sh
+# Parameters:
+# $1: Path to the internal cert with private key
+# $2: Path to the new keystore
+# $3: Keystore password
+function create_keystore()
+{
+    echo "Creating Microsoft keystore $2 from $1"
+    rm -f "$2"
+    ALIAS=$(basename "$1" .pem)
+    RANDFILE=/tmp/.rnd openssl pkcs12 -export -in "$1" -out "$2" -name "$ALIAS" -password pass:"$3" -certpbe aes-128-cbc -keypbe aes-128-cbc -macalg sha256
+}
 
-echo "Preparing truststore for replication listener"
-# Add each certificate to the trust store
-STORE=/tmp/kafka/cluster.truststore.p12
-rm -f "$STORE"
-for CRT in /opt/kafka/cluster-ca-certs/*.crt; do
-  ALIAS=$(basename "$CRT" .crt)
-  echo "Adding $CRT to truststore $STORE with alias $ALIAS"
-  create_truststore "$STORE" "$CERTS_STORE_PASSWORD" "$CRT" "$ALIAS"
-done
-echo "Preparing truststore for replication listener is complete"
+# Parameters:
+# $1: Path to the internal cert with private key
+# $2: Path to the new truststore
+# $3: Truststore password
+function create_truststore()
+{
+    echo "Creating Microsoft truststore $2 from $1"
 
-echo "Looking for the CA matching the server certificate"
-CA=$(find_ca /opt/kafka/cluster-ca-certs "/opt/kafka/broker-certs/$HOSTNAME.crt")
-
-if [ ! -f "$CA" ]; then
-    echo "No CA matching the server certificate found. This process will exit with failure."
-    exit 1
-fi
-echo "CA matching the server certificate found: $CA"
-
-echo "Preparing keystore for replication and clienttls listener"
-STORE=/tmp/kafka/cluster.keystore.p12
-rm -f "$STORE"
-create_keystore "$STORE" "$CERTS_STORE_PASSWORD" \
-    "/opt/kafka/broker-certs/$HOSTNAME.crt" \
-    "/opt/kafka/broker-certs/$HOSTNAME.key" \
-    "$CA" \
-    "$HOSTNAME"
-echo "Preparing keystore for replication and clienttls listener is complete"
-
-regex="^\/opt\/kafka\/certificates\/(custom|oauth)-(.+)-(.+)-certs$"
-for CERT_DIR in /opt/kafka/certificates/*; do
-  if [[ $CERT_DIR =~ $regex ]]; then
-    listener=${BASH_REMATCH[1]}-${BASH_REMATCH[2]}-${BASH_REMATCH[3]}
-    echo "Preparing store for $listener listener"
-    if [[ ${BASH_REMATCH[1]} == "custom"  ]]; then
-      echo "Creating keystore /tmp/kafka/$listener.keystore.p12"
-      rm -f /tmp/kafka/"$listener".keystore.p12
-      create_keystore_without_ca_file /tmp/kafka/"$listener".keystore.p12 "$CERTS_STORE_PASSWORD" "${CERT_DIR}/tls.crt" "${CERT_DIR}/tls.key" custom-key
-    elif [[ ${BASH_REMATCH[1]} == "oauth"  ]]; then
-      trusted_certs="STRIMZI_${BASH_REMATCH[2]^^}_${BASH_REMATCH[3]}_OAUTH_TRUSTED_CERTS"
-      if [ -n "${!trusted_certs}" ]; then
-        prepare_truststore "/tmp/kafka/$listener.truststore.p12" "$CERTS_STORE_PASSWORD" "$CERT_DIR" "${!trusted_certs}"
-      fi
+    # Disable FIPS if needed
+    if [ "$FIPS_MODE" = "disabled" ]; then
+        KEYTOOL_OPTS="${KEYTOOL_OPTS} -J-Dcom.redhat.fips=false"
+    else
+        KEYTOOL_OPTS=""
     fi
-    echo "Preparing store for ${BASH_REMATCH[1]} ${BASH_REMATCH[2]} listener is complete"  
-  fi
-done
 
-echo "Preparing truststore for client authentication"
-# Add each certificate to the trust store
-STORE=/tmp/kafka/clients.truststore.p12
-rm -f "$STORE"
-for CRT in /opt/kafka/client-ca-certs/*.crt; do
-  ALIAS=$(basename "$CRT" .crt)
-  echo "Adding $CRT to truststore $STORE with alias $ALIAS"
-  create_truststore "$STORE" "$CERTS_STORE_PASSWORD" "$CRT" "$ALIAS"
-done
-echo "Preparing truststore for client authentication is complete"
+    SCRATCH="/tmp/waimea-scratch"
+    mkdir -p $SCRATCH
+    BASE=$(basename "$1" .pem)
+    i=1
+    ALIAS="$BASE-$i"
+    TEMP="$SCRATCH/$ALIAS.pem"
 
-if [ -n "$STRIMZI_OPA_AUTHZ_TRUSTED_CERTS" ]; then
-  echo "Preparing Open Policy Agent authorization truststore"
-  prepare_truststore "/tmp/kafka/authz-opa.truststore.p12" "$CERTS_STORE_PASSWORD" "/opt/kafka/certificates/authz-opa-certs" "$STRIMZI_OPA_AUTHZ_TRUSTED_CERTS"
-fi
+    # openssl x509 imports first cert from stdin without closing or discarding
+    # the rest of the input, so we can feed the cert bundle into a loop
+    # via stdin and extract each public cert into its own TEMP file for processing
+    while openssl x509 -out $TEMP 2> /dev/null; do
+        keytool ${KEYTOOL_OPTS} -keystore "$2" -storepass "$3" -noprompt -import -file "$TEMP" -alias "$ALIAS" -storetype PKCS12
+        rm -f "$TEMP"
+        i=$((i+1))
+        ALIAS="$BASE-$i"
+        TEMP="$SCRATCH/$ALIAS.pem"
+    done < $1
+}
 
-if [ -n "$STRIMZI_KEYCLOAK_AUTHZ_TRUSTED_CERTS" ]; then
-  echo "Preparing Keycloak authorization truststore"
-  prepare_truststore "/tmp/kafka/authz-keycloak.truststore.p12" "$CERTS_STORE_PASSWORD" "/opt/kafka/certificates/authz-keycloak-certs" "$STRIMZI_KEYCLOAK_AUTHZ_TRUSTED_CERTS"
+echo "Microsoft Kafka Cert Preparation"
+
+KEYSTORE=/tmp/kafka/cluster.keystore.p12
+create_keystore "$MSFT_KAFKA_INTERNAL_CERT" "$KEYSTORE" "$CERTS_STORE_PASSWORD"
+
+TRUSTSTORE=/tmp/kafka/cluster.truststore.p12
+rm -f "$TRUSTSTORE"
+create_truststore "$MSFT_KAFKA_INTERNAL_CERT" "$TRUSTSTORE" "$CERTS_STORE_PASSWORD"
+
+if [ -n "$MSFT_KAFKA_EXTERNAL_CERT"]; then
+    KEYSTORE=/tmp/kafka/external.keystore.p12
+    create_truststore "$MSFT_KAFKA_EXTERNAL_CERT" "$KEYSTORE" "$CERTS_STORE_PASSWORD"
 fi
